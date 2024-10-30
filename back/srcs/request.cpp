9f9843e7396753e84f659a94ac1fb128cf44026c
Leo Giband <leo.giband@gmail.com>
2024-10-30 14:03:30 +0100

    Re-add set depth with params

--- back/srcs/request.cpp
+++ back/srcs/request.cpp
@@ -132,7 +132,7 @@ void r_ia(const httplib::Request &req, httplib::Response &res) {
 
     // create board
 
-    game.set_depth(1);
+    game.set_depth(request.depth);
     std::vector<int> threshold_by_layer = generate_thresholds(game.get_depth(), 50000, 50, 3);
     game.set_threshold(threshold_by_layer);
 
